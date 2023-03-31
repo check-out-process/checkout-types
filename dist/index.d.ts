c3fac705dac59d4c8a6b6e875022ac139f812f66
Amit Assa <amitassa9@gmail.com>
2023-04-01 01:42:24 +0300

    converted to typescript library

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from './lib/models/Bed';
+export * from './lib/models/Department';
+export * from './lib/models/Process';
+export * from './lib/models/Room';
+export * from './lib/models/Sector';
