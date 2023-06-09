5ee4fa43cf9ffeb151824b26eca71f94d10b850a
afekl <afekev55@gmail.com>
2023-06-09 17:58:04 +0300

    add chne status enum dist

--- dist/lib/enums/status.enum.js
+++ dist/lib/enums/status.enum.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Status = void 0;
 var Status;
 (function (Status) {
-    Status["Waiting"] = "\u05DE\u05D7\u05DB\u05D4 \u05DC\u05E9\u05D9\u05D1\u05D5\u05E5";
+    Status["Waiting_Assigning"] = "\u05DE\u05D7\u05DB\u05D4 \u05DC\u05E9\u05D9\u05D1\u05D5\u05E5";
+    Status["Waiting_Confirm"] = "\u05DE\u05D7\u05DB\u05D4 \u05DC\u05D0\u05D9\u05E9\u05D5\u05E8";
     Status["In_Progress"] = "\u05D1\u05EA\u05D4\u05DC\u05D9\u05DA";
     Status["Done"] = "\u05D4\u05E1\u05EA\u05D9\u05D9\u05DD";
 })(Status = exports.Status || (exports.Status = {}));
